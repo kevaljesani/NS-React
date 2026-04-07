5821b5b413fa7ac5c62c230c484ed2477580d618
coder <65643094+kevaljesani@users.noreply.github.com>
2026-04-07 21:36:47 +0530

    Add files via upload
    EP-1

---
+++ EP-1/App.js
@@ -0,0 +1,31 @@
+//  <div class="parent1">
+//     <div class="child">
+//         <h1>Heading</h1>
+//     </div>
+// </div> 
+//  <div class="parent2">
+//     <div class="child">
+//         <h1>Heading</h1>
+//     </div>
+// </div> 
+
+
+const temp = React.createElement([
+    'div',{className:'parent1'},
+    React.createElement('div',{className:'child1'},
+        React.createElement('h1',null,"Headding")
+    ),
+    'div',{className:'parent2'},
+    React.createElement('div',{className:'child2'},
+        React.createElement('h1',null,"Headding")
+    )
+]
+)
+console.log(temp)
+
+
+
+
+const heading = React.createElement('h1', {id:'heading',pageTitle:"Title"}, "Hello world from React!");
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
